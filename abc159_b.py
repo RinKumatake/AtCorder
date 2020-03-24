25a6b9eca87a86e12400904b73206587271c7b05
pandanoranran <pandanoranran658@gmail.com>
2020-03-24 23:08:04 +0900

    文字列が回文であるかどうかを判定する。更に強い回文であるかどうかを判定する

---
+++ abc159_b.py
@@ -0,0 +1,21 @@
+#回文であるかどうかを判定
+def palindrome(s:str)->str:
+    #文字列sの長さをnとおく
+    s_reverse = s[::-1]     
+    x = (len(s) - 1) // 2
+    s_1_x = s[:x] #文字列sの1文字目からx文字目までを求める
+    s_1_x_reverse = s_1_x[::-1] #1文字目からx文字目までを反転
+    y = (len(s) + 3)//2
+    s_1_y = s[y - 1:len(s)] #文字列sのy文字目からn文字目まで
+    s_1_y_reverse = s_1_y[::-1] #y文字目からn文字目まで       
+    
+    if s == s_reverse:
+        if s_1_x == s_1_x_reverse and s_1_y == s_1_y_reverse:
+            return "Yes"
+        else:
+            return "No"
+    else:
+        return "No"
+
+s = input()
+print(palindrome(s))
