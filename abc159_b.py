2096acde88c6bdb15d52495cd3d663bc62c80c0d
pandanoranran <pandanoranran658@gmail.com>
2020-03-25 11:28:06 +0900

    回文を判定する

--- abc159_b.py
+++ abc159_b.py
@@ -1,4 +1,4 @@
-#回文であるかどうかを判定
+#文字数が奇数であるS（文字列）が回文であるかどうかを判定
 def palindrome(s:str)->str:
     #文字列sの長さをnとおく
     s_reverse = s[::-1]     
@@ -9,8 +9,8 @@ def palindrome(s:str)->str:
     s_1_y = s[y - 1:len(s)] #文字列sのy文字目からn文字目まで
     s_1_y_reverse = s_1_y[::-1] #y文字目からn文字目まで       
     
-    if s == s_reverse:
-        if s_1_x == s_1_x_reverse and s_1_y == s_1_y_reverse:
+    if s == s_reverse: #sが回文かどうか判定
+        if s_1_x == s_1_x_reverse and s_1_y == s_1_y_reverse: #強い回文かどうか判定
             return "Yes"
         else:
             return "No"
