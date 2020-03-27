3dc5e5110a3d7368a294e2762dc14b92f15dcdd0
pandanoranran <pandanoranran658@gmail.com>
2020-03-27 22:37:30 +0900

    次の日の天気を予想

---
+++ abc141_a.py
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc141/tasks/abc141_a
+s = input()
+if s == 'Sunny':
+    print('Cloudy')
+elif s == 'Cloudy':
+    print('Rainy')
+elif s == ('Rainy'):
+    print('Sunny')
