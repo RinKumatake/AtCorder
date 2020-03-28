34904575ff6b0017044255437cbffe936692ba8b
pandanoranran <pandanoranran658@gmail.com>
2020-03-28 13:15:37 +0900

    1ijou

---
+++ abc083_b.py
@@ -0,0 +1,20 @@
+#https://atcoder.jp/contests/abc083/tasks/abc083_b
+#1からnまでの各桁の和を求める関数
+def sum_s(s:int) -> int:
+    """1からnまでの各桁の和を求める
+    :param s: 数字
+    :return: sの各桁の合計
+    """
+    s = str(s)
+    s_array = list(map(int, s))
+    return sum(s_array)
+
+
+
+n, a, b = map(int, input().split())
+ans = []
+for i in range(1, n + 1):
+    sum_array = sum_s(i)
+    if sum_array >= a and sum_array <= b:
+        ans.append(i)
+print(sum(ans))
