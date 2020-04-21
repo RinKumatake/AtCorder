7937b3c2bbdb79f1b8af1d93db2b3ebce8281702
pandanoranran <pandanoranran658@gmail.com>
2020-04-21 14:52:09 +0900

    投票数の多かったワードを辞書順に出力

---
+++ abc155/abc155_c.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc155/tasks/abc155_c
+import collections
+import itertools
+
+n = int(input())
+word = [input() for i in range(n)]
+counter = collections.Counter(word)#出てきた言葉と回数が辞書型で返る
+rank_word = counter.most_common() #most_common()メソッドを使うことで回数が多い順にタプル型で返る
+most_common = rank_word[0][1]
+most_common_lst = [i[0] for i in rank_word if i[1] == most_common]#同率１位のワードをリストに
+lst = sorted(most_common_lst) #ワードを辞書順に
+for i in lst:
+    print(i)
