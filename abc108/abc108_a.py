12a51886e859152e612ef010a731cbc8bc9cc1ef
pandanoranran <pandanoranran658@gmail.com>
2020-04-22 15:38:32 +0900

    1からkまでの数字で偶数と奇数のペアは何組できるか。偶奇の順番は問わない

---
+++ abc108/abc108_a.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abc108/tasks/abc108_a
+k = int(input())
+even = [i for i in range(1, k+1) if i%2==0]
+odd = [i for i in range(1, k+1) if i%2!=0]
+ans_lst = []
+for i in even:    
+    for j in odd:
+        even_odd = []
+        even_odd.append(i)
+        even_odd.append(j)
+        ans_lst.append(even_odd)
+print(len(ans_lst))
+
+
