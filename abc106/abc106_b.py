690ee5bf1082ea8182a0492065c47526b83a91cc
pandanoranran <pandanoranran658@gmail.com>
2020-04-23 15:59:49 +0900

    1からnまでの奇数で約数が8個あるものの個数を出力する

---
+++ abc106/abc106_b.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abc106/tasks/abc106_b
+  
+n = int(input())
+ans = 0
+for i in range(1, n+1, 2):
+    count = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            count+=1
+    if count == 8:
+        ans += 1
+print(ans)
